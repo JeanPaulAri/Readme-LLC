68ad1335304f26fbc6947273a57e55bbbac2d730
Jean Paul Ari Maldonado <jean.ari@ucsp.edu.pe>
2024-05-16 12:53:41 -0500

    dev/ fixing parser

--- main.cpp
+++ main.cpp
@@ -7,21 +7,38 @@
 #include <set>
 #include <list>
 #include <sstream>
+#include <fstream>
 
 using namespace std;
 
-map<string, string> tokenTypes = {
-    {"print", "PRINT_KEY"},
-    {"=", "ASSIGN_OP"},
-    {"(", "OPEN_PAR"},
-    {")", "CLOSE_PAR"},
-    {"+", "ADD_OP"},
-    {"$", "EOP"}
+enum class State{
+    START,
+    H1,
+    H2,
+    H3,
+    ITALIC,
+    BOLD,
+    UNDERLINE,
+    TEXT,
+    PRINT_KEY,
+    ASSIGN_OP,
+    OPEN_PAR,
+    CLOSE_PAR,
+    ADD_OP,
+    END
 };
 
-// map<string,string> tokenMd = {
-//     {"#","TITLE"}, 
-// }
+enum class Action {
+    Shift,
+    Reduce,
+    Accept,
+    Error
+};
+
+struct ParseTableEntry {
+    Action action;
+    State nextState;
+};
 
 struct Token{
     string type;
@@ -30,43 +47,201 @@ struct Token{
     int line, column;
 };
 
-auto scan = [](string input) -> vector<Token>{
+map<State, map<string, ParseTableEntry>> parseTable = {
+    {State::START, {
+        {"H1", {Action::Shift, State::H1}},
+        {"H2", {Action::Shift, State::H2}},
+        {"H3", {Action::Shift, State::H3}},
+        {"ITALIC", {Action::Shift, State::ITALIC}},
+        {"BOLD", {Action::Shift, State::BOLD}},
+        {"UNDERLINE", {Action::Shift, State::UNDERLINE}},
+        {"TEXT", {Action::Shift, State::TEXT}},
+        {"$", {Action::Accept, State::END}}
+    }},
+    {State::H1, {
+        {"TEXT", {Action::Shift, State::TEXT}},
+        {"$", {Action::Reduce, State::START}}
+    }},
+    {State::H2, {
+        {"TEXT", {Action::Shift, State::TEXT}},
+        {"$", {Action::Reduce, State::START}}
+    }},
+    {State::H3, {
+        {"TEXT", {Action::Shift, State::TEXT}},
+        {"$", {Action::Reduce, State::START}}
+    }},
+    {State::ITALIC, {
+        {"TEXT", {Action::Shift, State::TEXT}},
+        {"$", {Action::Reduce, State::START}}
+    }},
+    {State::BOLD, {
+        {"TEXT", {Action::Shift, State::TEXT}},
+        {"$", {Action::Reduce, State::START}}
+    }},
+    {State::UNDERLINE, {
+        {"TEXT", {Action::Shift, State::TEXT}},
+        {"$", {Action::Reduce, State::START}}
+    }},
+    {State::TEXT, {
+        {"H1", {Action::Reduce, State::START}},
+        {"H2", {Action::Reduce, State::START}},
+        {"H3", {Action::Reduce, State::START}},
+        {"ITALIC", {Action::Reduce, State::START}},
+        {"BOLD", {Action::Reduce, State::START}},
+        {"UNDERLINE", {Action::Reduce, State::START}},
+        {"$", {Action::Reduce, State::START}}
+    }}
+};
 
-    vector<Token> tokens;
-    istringstream iss(input);
+map<string, string> tokenTypes = {
+    {"#", "H1"},
+    {"##", "H2"},
+    {"###", "H3"},
+    {"*", "ITALIC"},
+    {"**", "BOLD"},
+    {"__", "UNDERLINE"},
+    {"print", "PRINT_KEY"},
+    {"=", "ASSIGN_OP"},
+    {"(", "OPEN_PAR"},
+    {")", "CLOSE_PAR"},
+    {"+", "ADD_OP"},
+    {"$", "EOP"}
+};
 
-    string word;
-    int input_line = 1, input_column = 1;
 
-    while(iss>> word){
-        Token token;
+struct Node{
+    string type;
+    string value;
+    vector<Node> children;
+};
 
-        bool isInt = true;
-        for (char symbol: word){
-            if (!isdigit(symbol)){
-                isInt = false;
-                break;
-            }
+auto parse = [](vector<Token> tokens) -> Node{
+    Node root;
+    root.type = "PROGRAM";
+    
+    for(auto& token:tokens){
+        Node node;
+        node.type = token.type;
+        node.value = token.value;
+
+        if(tokenTypes.find(token.type) != tokenTypes.end()){
+            root.children.push_back(node);
         }
 
-        if( tokenTypes.find(word) != tokenTypes.end()){
-            token.type = tokenTypes[word];
-        }else if(isInt){
-            token.type = "INT";
-        }else{
-                token.type = "ID";
-        }
-
-        token.value = word;
-        token.line = input_line;
-        token.column = input_column;
-
-        tokens.push_back(token);
-
-        input_column += word.length() +1;
     }
 
+    return root;
+};
 
+auto parse2 = [](vector<Token> tokens) -> Node {
+    Node root;
+    root.type = "PROGRAM";
+
+    State currentState = State::START;
+    for(auto& token:tokens){
+        if(parseTable[currentState].find(token.type) == parseTable[currentState].end()){
+            cerr << "Error en (" << token.line << ":" << token.column << "): token inesperado '" << token.value << "'\n";
+            exit(1);
+        }
+
+        ParseTableEntry entry = parseTable[currentState][token.type];
+        switch(entry.action){
+            case Action::Shift:
+                currentState = entry.nextState;
+                break;
+            case Action::Reduce:
+                // Aquí debes implementar la lógica para reducir la pila de análisis
+                break;
+            case Action::Accept:
+                return root;
+            case Action::Error:
+                cerr << "Error en (" << token.line << ":" << token.column << "): token inesperado '" << token.value << "'\n";
+                exit(1);
+        }
+
+        Node node;
+        node.type = token.type;
+        node.value = token.value;
+        root.children.push_back(node);
+    }
+
+    return root;
+};
+string input;
+size_t pos = 0;
+int line = 1, column = 1;
+
+auto getchr = []() -> char {
+      if (pos >= input.size()) {
+        return '\0';
+    }
+    char ch = input[pos++];
+    if (ch == '\n') {
+        line++;
+        column = 1;
+    } else {
+        column++;
+    }
+    return ch;
+};
+
+auto peekchar = []() -> char {
+    if (pos >= input.size()) {
+        return '\0';
+    }
+    return input[pos];
+};
+
+auto getToken = []() -> Token {
+       char ch;
+    while ((ch = getchr()) != '\0' && isspace(ch)) {
+        // Ignora los espacios en blanco
+    }
+
+    if (ch == '\0') {
+        return {"$", "", line, column};
+    }
+
+    Token token;
+    token.line = line;
+    token.column = column - 1;
+
+    string str(1, ch);
+    if (tokenTypes.find(str) != tokenTypes.end()) {
+        // Si el carácter es el inicio de un token de marcado, lee el resto del token
+        string nextStr = str + peekchar();
+        while (tokenTypes.find(nextStr) != tokenTypes.end()) {
+            str += getchr();
+            nextStr = str + peekchar();
+        }
+        token.type = tokenTypes[str];
+        token.value = str;
+    } else if (isdigit(ch)) {
+        while (isdigit(ch = peekchar())) {
+            str += getchr();
+        }
+        token.type = "INT";
+        token.value = str;
+    } else if (isalpha(ch)) {
+        while (isalpha(ch = peekchar()) || ch == ' ') { // Agrega un espacio a los caracteres permitidos para el texto sin formato
+            str += getchr();
+            ch = peekchar();
+        }
+        token.type = "ID"; 
+        token.value = str;
+    } else { // Si el carácter no es el final de la cadena
+        cerr << "Error en (" << line << ":" << column << "): caracter inesperado '" << ch << "'\n";
+        exit(1);
+    }
+
+    return token;
+};
+auto scan = []() -> vector<Token> {
+    vector<Token> tokens;
+    Token token;
+    while ((token = getToken()).type != "$") {
+        tokens.push_back(token);
+    }
     return tokens;
 };
 
@@ -90,17 +265,31 @@ void printTokenMap(map<string, string> tokenMap) { //function to validate map of
     }
 }
 
-int main(){
-    string input = "a = 2 + 7\nprint(a) # este es un comentario\n$";
-
-    // Escanear el string
-    vector<Token> tokens = scan(input);
-
-    // Imprimir información de los tokens
+auto printTokens = [](const vector<Token>& tokens) {
     cout << "INFO SCAN - Start scanning...\n";
     for (const auto& token : tokens) {
         cout << "DEBUG SCAN - " << token.type << " [ " << token.value << " ] found at (" << token.line << ":" << token.column << ")\n";
     }
     cout << "INFO SCAN - Completed with 0 errors\n";
+};
+
+int main(){
+    ifstream file("input.txt");
+
+    if (!file) {
+    cerr << "No se pudo abrir el archivo input.txt";
+    return 1; // Retorna un código de error
+    }
+
+     input = string((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
+
+    vector<Token> tokens;
+    Token token;
+    while ((token = getToken()).type != "$") {
+        tokens.push_back(token);
+    }
+
+    Node ast = parse(tokens);
+    printTokens(tokens);
     return 0;
 }
